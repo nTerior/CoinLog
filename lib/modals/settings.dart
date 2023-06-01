7fb450504b3b8461a6ff18a0e23be820d33d4c42
Paul Stier <paul-stier@gmx.de>
2023-06-01 19:49:54 +0200

    Removed overscroll indicator

--- lib/modals/settings.dart
+++ lib/modals/settings.dart
@@ -11,49 +11,55 @@ class SettingsModal extends StatelessWidget {
   Widget build(BuildContext context) {
     final settings = Provider.of<Settings>(context, listen: false);
 
-    return ListView(
-      shrinkWrap: true,
-      children: [
-        Text(
-          "Settings",
-          style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                fontWeight: FontWeight.w100,
-              ),
-        ),
-        Divider(color: Colors.white.withOpacity(0.1)),
-        const Text("Limits", style: TextStyle(fontWeight: FontWeight.w100)),
-        ToggleableSettingsInputTile(
-          title: "Total Balance Limit",
-          suffix: "€",
-          type: TextInputType.number,
-          initialValue: settings.balanceLimit,
-          initiallyEnabled: settings.balanceLimitEnabled,
-          onEdit: (value) => settings.balanceLimit =
-              value.isNotEmpty ? double.parse(value) : null,
-          onToggle: (value) => settings.balanceLimitEnabled = value,
-        ),
-        ToggleableSettingsInputTile(
-          title: "Weekly Limit",
-          suffix: "€",
-          type: TextInputType.number,
-          initialValue: settings.weeklyLimit,
-          initiallyEnabled: settings.weeklyLimitEnabled,
-          onEdit: (value) => settings.weeklyLimit =
-              value.isNotEmpty ? double.parse(value) : null,
-          onToggle: (value) => settings.weeklyLimitEnabled = value,
-        ),
-        ToggleableSettingsInputTile(
-          title: "Monthly Limit",
-          suffix: "€",
-          type: TextInputType.number,
-          initialValue: settings.monthlyLimit,
-          initiallyEnabled: settings.monthlyLimitEnabled,
-          onEdit: (value) => settings.monthlyLimit =
-              value.isNotEmpty ? double.parse(value) : null,
-          onToggle: (value) => settings.monthlyLimitEnabled = value,
-        ),
-        const SizedBox(height: Layout.padding),
-      ],
+    return NotificationListener<OverscrollIndicatorNotification>(
+      onNotification: (notification) {
+        notification.disallowIndicator();
+        return false;
+      },
+      child: ListView(
+        shrinkWrap: true,
+        children: [
+          Text(
+            "Settings",
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  fontWeight: FontWeight.w100,
+                ),
+          ),
+          Divider(color: Colors.white.withOpacity(0.1)),
+          const Text("Limits", style: TextStyle(fontWeight: FontWeight.w100)),
+          ToggleableSettingsInputTile(
+            title: "Total Balance Limit",
+            suffix: "€",
+            type: TextInputType.number,
+            initialValue: settings.balanceLimit,
+            initiallyEnabled: settings.balanceLimitEnabled,
+            onEdit: (value) => settings.balanceLimit =
+                value.isNotEmpty ? double.parse(value) : null,
+            onToggle: (value) => settings.balanceLimitEnabled = value,
+          ),
+          ToggleableSettingsInputTile(
+            title: "Weekly Limit",
+            suffix: "€",
+            type: TextInputType.number,
+            initialValue: settings.weeklyLimit,
+            initiallyEnabled: settings.weeklyLimitEnabled,
+            onEdit: (value) => settings.weeklyLimit =
+                value.isNotEmpty ? double.parse(value) : null,
+            onToggle: (value) => settings.weeklyLimitEnabled = value,
+          ),
+          ToggleableSettingsInputTile(
+            title: "Monthly Limit",
+            suffix: "€",
+            type: TextInputType.number,
+            initialValue: settings.monthlyLimit,
+            initiallyEnabled: settings.monthlyLimitEnabled,
+            onEdit: (value) => settings.monthlyLimit =
+                value.isNotEmpty ? double.parse(value) : null,
+            onToggle: (value) => settings.monthlyLimitEnabled = value,
+          ),
+          const SizedBox(height: Layout.padding),
+        ],
+      ),
     );
   }
 }
